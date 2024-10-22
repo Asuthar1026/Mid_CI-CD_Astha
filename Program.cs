78057a76bf82a777d52dafe5cd47b7ab71e43de0
Astha <asthasuthar888@gmail.com>
2024-10-22 17:19:58 -0400

    added files

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Mid_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is Astha Suthar-8906626, i created this project for Mid Exam of CI/CD");
+        }
+    }
+}
